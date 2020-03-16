c8d3e733b2fb32006c6b3675e727e3a64a41f400
Raoux <killian@semifir.com>
2020-03-16 15:01:30 +0100

    Request Body

---
+++ Controllers/CalculatriceController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatriceAPI.Controllers
+{
+    using DTO;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatriceController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Is Ok";
+        }
+
+        [HttpPost]
+        public string Post([FromBody] Data content)
+        {
+            string res;
+            switch (content.operation)
+            {
+                case "AND":
+                    res = (content.a + content.b).ToString();
+                    break;
+                case "MUL":
+                    res = (content.a * content.b).ToString();
+                    break;
+                case "DIV":
+                    res = (content.a / content.b).ToString();
+                    break;
+                case "SUB":
+                    res = (content.a - content.b).ToString();
+                    break;
+                default:
+                    res = $"Operation {content.operation} n'est pas definie!";
+                    break;
+            }
+            return res;
+        }
+
+        [HttpGet]
+        [Route("add/{a}/{b}")]
+        public string Addition(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+    }
+}
